1cd714e4d1e2ec8abf3474b52366991bdecf0ba8
chiga18 <gbd754@gmail.com>
2019-10-16 20:44:26 +0500

    Загрузка версти ActiveBox

---
+++ js/common.js
@@ -0,0 +1,33 @@
+$(function(){
+    // Плавный скролл до элемента
+
+    $("[data-scroll]").on("click", function(event){
+        event.preventDefault();
+
+        let elementId = $(this).data('scroll');
+
+
+        let elementOffset = $(elementId).offset().top;
+        console.log(elementOffset);
+
+
+        $("html, body").animate({
+            scrollTop: elementOffset
+        },1000);
+        // Настрйока скорости скролла   
+    });
+
+
+    // Мобильная навигация
+        let nav = $("#nav");
+        let navToggle = $("#navToggle");
+
+        navToggle.on("click", function(event){
+            event.preventDefault();
+
+            nav.toggleClass("show");
+        });
+});
+
+
+
